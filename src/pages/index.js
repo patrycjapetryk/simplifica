eaa1f5d4546ae367a499bb6a6f568445f5a929af
Patrycja Petryk <patrycja.petryk@gmail.com>
2020-04-10 20:50:27 +0200

    feat: homepage structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,89 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 // import styled from "styled-components"
 
-const IndexPage = () => {
+export const query = graphql`
+  {
+    prismic {
+      allHomepages {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_HomepageBodySlider {
+                type
+                primary {
+                  title
+                  paragraph
+                  image
+                }
+              }
+              ... on PRISMIC_HomepageBodyWhat_i_do {
+                type
+                primary {
+                  title
+                  paragraph
+                }
+                fields {
+                  image
+                  title
+                  paragraph
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const {
+    prismic: {
+      allHomepages: {
+        edges: [
+          {
+            node: { body },
+          },
+        ],
+      },
+    },
+  } = data
+
   return (
     <>
-      <h1>Homepage</h1>
-      <p>Welcome to my homepage! </p>
+      {body.map((item, index) => {
+        return (
+          <div key={index}>
+            <h2>{item.primary.title[0].text}</h2>
+            <p>
+              {item.primary.paragraph[0].text
+                ? item.primary.paragraph[0].text
+                : ''}
+            </p>
+            {item.primary.image && (
+              <img
+                src={item.primary.image.url}
+                width={item.primary.image.dimensions.width}
+                alt={item.primary.image.alt}
+              />
+            )}
+
+            {item.fields &&
+              item.fields.map((slide, slideIndex) => (
+                <div key={slideIndex}>
+                  <img
+                    src={slide.image.url}
+                    width={slide.image.dimensions.width}
+                    alt={slide.image.alt}
+                  />
+                  <h3>{slide.title[0].text}</h3>
+                  <p>{slide.paragraph[0].text}</p>
+                </div>
+              ))}
+          </div>
+        )
+      })}
     </>
   )
 }
