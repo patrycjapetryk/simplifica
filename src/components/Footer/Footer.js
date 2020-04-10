5dd8dd2bd8c1e256163b9d7b5037b1205694144e
Patrycja Petryk <patrycja.petryk@gmail.com>
2020-04-10 23:32:18 +0200

    feat: add footer query

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react'
-// import styled from "styled-components"
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Footer = () => (
-  <footer>
-    <p>footer</p>
-  </footer>
-)
+import Navigation from '../Navigation/Navigation'
+
+const StyledFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  text-align: left;
+`
+
+const StyledParagraph = styled.p`
+  margin-left: 100px;
+`
+
+const Footer = () => {
+  const { prismic } = useStaticQuery(
+    graphql`
+      query {
+        prismic {
+          allFooters {
+            edges {
+              node {
+                text
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const node = prismic.allFooters.edges[0].node
+  return (
+    <StyledFooter>
+      <Navigation />
+      <StyledParagraph>
+        {`©${new Date().getFullYear()} ${node.text[0].text}`}
+      </StyledParagraph>
+    </StyledFooter>
+  )
+}
 
 export default Footer
