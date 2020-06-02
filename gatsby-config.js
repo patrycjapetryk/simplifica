e97fa6a9aa8a7e9bb088e0f9a0b07c8764bbea18
Patrycja Petryk <patrycja.petryk@gmail.com>
2020-06-02 20:28:29 +0200

    feat: add manifest with icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,18 +47,60 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `simplifica - strony internetowe i sklepy`,
+        short_name: `simplifica`,
+        start_url: `/`,
+        background_color: `#F0F6FC`,
+        theme_color: `#4A658A`,
+        display: `minimal-ui`,
+        // icons: [
+        //   {
+        //     src: 'src/assets/icons/icon-72x72.png',
+        //     sizes: '72x72',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-96x96.png',
+        //     sizes: '96x96',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-128x128.png',
+        //     sizes: '128x128',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-144x144.png',
+        //     sizes: '144x144',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-152x152.png',
+        //     sizes: '152x152',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-384x384.png',
+        //     sizes: '384x384',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'src/assets/icons/icon-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        // ],
+        icon: `src/assets/icons/icon-512x512.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
